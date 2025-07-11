51e75849d7f6a955c276b545f085ee32aa201553
Huynh <tsamanh99@gmail.com>
2025-07-11 17:16:13 -0500

    export all R-squared

--- plspm/bootstrap_with_seed.py
+++ plspm/bootstrap_with_seed.py
@@ -288,7 +288,7 @@ class Bootstrap:
 
         self.__weights = _create_summary(weights, outer_model.model().loc[:, "weight"])
         self.__r_squared = _create_summary(r_squared, self.__original_inner_model.r_squared()).loc[self.__original_inner_model.endogenous(), :]
-        self.__r_square_list = r_squared
+        self.__r_squared_list = r_squared
         self.__total_effects = _create_summary(total_effects, self.__original_inner_model.effects().loc[:, "total"])
         self.__paths = _create_summary(paths, self.__original_inner_model.effects().loc[:, "direct"])
         self.__loading = _create_summary(loadings, outer_model.model().loc[:, "loading"])
